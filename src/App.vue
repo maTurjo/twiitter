743c163d683b433ce4124df17c54627ff439b9e8
maTurjo <ma.turjo@gmail.com>
2021-05-04 01:59:44 +0600

    first scratch

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div id="app">
+
+  {{fullName}} @ {{user.username}}
+  <br>
+  {{followers}}
+  <br>
+  <button  @click="followUser();check()" name="Follow">Follow</button>
+  <br><br>
+  <span style="border:3px solid red">{{msg}}</span>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      followers:0,
+      user:{
+        id:1,
+        username:'_Turjo',
+        firstName:'Mubassir Ahmed',
+        lastName:'Turjo',
+        email:'ma.turjo@gmail.com',
+        isAdmin: true
+      },
+      msg:""
+    }
+  },
+  watch:{
+    followers(n,o){
+      o;
+      if(n%2==0){
+        this.msg="You Just Got A New FOLLOWER ! ! (even)";
+      }
+      else this.msg="You Just Got A New FOLLOWER ! ! (Odd)";
+    }
+
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+
+
+  },
+  methods:{
+    followUser(){
+      this.followers++;
+    },
+    check(){
+      if(this.followers>=10){this.followers=0;}
+    }
+
+
+  },
+
+  mounted(){
+  //  this.followUser();
+   // this.followUser();
   }
 }
 </script>
@@ -17,6 +68,7 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: xx-large;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/profileCard.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div id=profileCard>
+        <span>{{fullName}}</span>
+
+    </div>
+
+
+</template>
+
+<script>
+
+export default{
+    name:"profileCard",
+    data(){
+        return{
+
+            user:{
+                Id:1,
+                Name:"tj_on_the_rocks_",
+                firstName:"Mubassir Ahmed",
+                lastName:"Turjo",
+                followers:0
+            }
+        }
+    },
+
+    computed:{
+        fullName(){
+            return `$this.user.firstName $this.user.lastName`;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
